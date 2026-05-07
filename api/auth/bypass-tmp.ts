bf221c89f0a313ec18c5d07490e6004a9ec57073
Kamil Klubuntu <49614906+Klubuntu@users.noreply.github.com>
2026-05-07 06:24:38 +0000

    [Fix] Bypass tmp script

--- api/auth/bypass-tmp.ts
+++ api/auth/bypass-tmp.ts
@@ -1,6 +1,6 @@
 import { handleCors } from '../_cors'
 
-export default async function handler(req, res) {
+export default async function handler(req: any, res: any) {
   if (handleCors(req, res)) return
 
   // Only allow in development
